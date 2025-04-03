3fac007f3a0abb4f49b5c22ed526d4c665e60305
4cplayz <tommyjamestremblay@gmail.com>
2025-04-03 11:14:39 -0400

    index.js fixes

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-import express from "express"
-import path from "path" // Add this import for better path handling
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const app = express()
+// Get the directory name in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Serve static files from the public directory
-app.use(express.static("C:/Users/tommy/OneDrive/Documents/Prog/WebSite/Web Developpement/TPG_web/public"))
+const app = express();
 
+// Serve static files from the public directory using absolute paths
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile("C:/Users/tommy/OneDrive/Documents/Prog/WebSite/Web Developpement/TPG_web/public/index.html")
-})
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.get('/information', (req, res) => {
-  res.sendFile("C:/Users/tommy/OneDrive/Documents/Prog/WebSite/Web Developpement/TPG_web/public/information.html")
-})
+  res.sendFile(path.join(__dirname, "public", "pages", "information.html"));
+});
 
-// Make sure your 404.html file is in the correct location
-// Either move it to the public directory or use the correct path
-
-// This should be the LAST route
-app.use((req, res, next) => {
-  res.status(404).sendFile("C:/Users/tommy/OneDrive/Documents/Prog/WebSite/Web Developpement/TPG_web/public/404.html")
-})
+// This should be the LAST route - 404 handler
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+});
 
 app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/')
-})
+  console.log('Server running at http://localhost:3000/');
+});
