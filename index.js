30bb9ce34b2873f30b0278b18206e1b6d4f8c811
4cplayz <tommyjamestremblay@gmail.com>
2025-04-03 10:57:18 -0400

    magic image

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import express from "express"
+import path from "path" // Add this import for better path handling
 
-const app = express() // Remove 'new' keyword
+const app = express()
 
-app.get('/', (req, res) => { // Add 'req' parameter
+// Serve static files from the public directory
+app.use(express.static("C:/Users/tommy/OneDrive/Documents/Prog/WebSite/Web Developpement/TPG_web/public"))
+
+app.get('/', (req, res) => {
   res.sendFile("C:/Users/tommy/OneDrive/Documents/Prog/WebSite/Web Developpement/TPG_web/public/index.html")
 })
 
-app.get('/information', (req, res) => { // Add 'req' parameter
+app.get('/information', (req, res) => {
   res.sendFile("C:/Users/tommy/OneDrive/Documents/Prog/WebSite/Web Developpement/TPG_web/public/information.html")
+})
+
+// Make sure your 404.html file is in the correct location
+// Either move it to the public directory or use the correct path
+
+// This should be the LAST route
+app.use((req, res, next) => {
+  res.status(404).sendFile("C:/Users/tommy/OneDrive/Documents/Prog/WebSite/Web Developpement/TPG_web/public/404.html")
 })
 
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000/')
 })
-
