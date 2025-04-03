bc3ee1e7af2183ad45a45bda932e69119675347e
4cplayz <tommyjamestremblay@gmail.com>
2025-04-03 10:51:03 -0400

    basic shit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import express from "express"
 
-const app = new express()
+const app = express() // Remove 'new' keyword
+
+app.get('/', (req, res) => { // Add 'req' parameter
+  res.sendFile("C:/Users/tommy/OneDrive/Documents/Prog/WebSite/Web Developpement/TPG_web/public/index.html")
+})
+
+app.get('/', (req, res) => { // Add 'req' parameter
+  res.sendFile("C:/Users/tommy/OneDrive/Documents/Prog/WebSite/Web Developpement/TPG_web/public/information.html")
+})
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/')
+})
+
