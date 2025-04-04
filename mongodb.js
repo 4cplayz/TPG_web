81e8f0d7f822ace485f807a26d597909a16e3ca3
4cplayz <tommyjamestremblay@gmail.com>
2025-04-04 08:47:31 -0400

    here is my api key ;)

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const url = "mongodb+srv://tommyjamestremblay:<db_password>@cluster0.wfxpiop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const url = "mongodb+srv://tommyjamestremblay:5j7VmBs9Sg2xgZXx@cluster0.wfxpiop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 mongoose.connect(url)
   .then(() => {
