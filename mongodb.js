2569a21067aecaf5119f527ee1ebd30c0516e772
4cplayz <tommyjamestremblay@gmail.com>
2025-04-04 09:10:57 -0400

    mogo db trouble shooting

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,27 @@
-import mongoose from "mongoose";
 
-const url = "mongodb+srv://tommyjamestremblay:5j7VmBs9Sg2xgZXx@cluster0.wfxpiop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://tommyjames:s9Q6pk1oghRx4Skw@cluster0.wfxpiop.mongodb.net/?appName=Cluster0";
 
-mongoose.connect(url)
-  .then(() => {
-    console.log("connected to dB")
-  }).catch((error) => ( console.log("connection error")))
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
