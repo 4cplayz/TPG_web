ae30c91e16db9de2be48cb25dcf48afc15806009
4cplayz <tommyjamestremblay@gmail.com>
2025-04-10 11:15:30 -0400

    api facts teste

--- api_route.js
+++ api_route.js
@@ -48,3 +48,51 @@ routeAPI.delete('/equipements/:id', async (req, res) => {
     console.log("Deletion did not work")
   }
 })
+
+routeAPI.get('/facts', async (req, res) => {
+  try {
+    const facts = await mesFacts.find({})
+    res.json(facts)
+    console.log("Found facts")
+  } catch (err) {
+    console.log('erreur', err)
+    res.json([])
+  }
+})
+
+routeAPI.get('/facts/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const fact = await mesFacts.findOneById(id)
+    res.json(fact)
+  } catch (err) {
+    console.log('erreur', err)
+    res.json({})
+  }
+})
+
+routeAPI.post('/facts', async (req, res) => {
+  try {
+    const body = req.body
+    console.log("New fact:", body)
+    await mesFacts.create(body)
+    res.send("New Sahara fact was created")
+  } catch (err) {
+    console.log('erreur', err)
+    console.log("Fact creation did not work")
+    res.json({})
+  }
+})
+
+routeAPI.delete('/facts/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    console.log("Deleting fact with id:", id)
+    const factToDelete = await mesFacts.findOneById(id)
+    await mesFacts.deleteOne({ _id: id })
+    res.send("Fact was successfully deleted")
+  } catch (err) {
+    console.log('erreur', err)
+    console.log("Fact deletion did not work")
+  }
+})
