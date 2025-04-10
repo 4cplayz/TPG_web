809296544da04fc06347108db60cd7933e0ec2c5
4cplayz <tommyjamestremblay@gmail.com>
2025-04-10 09:53:50 -0400

    piss shit wow

--- api_route.js
+++ api_route.js
@@ -9,7 +9,28 @@ routeAPI.get('/equipements',async (req,res)=>{
   console.log("found equipements")
 })
 
-routeAPI.get('/equipements/:id', (req,res)=>{
-const id = req.params.id
+routeAPI.get('/equipements/:id',async (req, res) => {
+  const id = req.params.id
+  try {
+    const equip = await mesEquipements.findOneById(id)
+    res.json(equip)
+  }
+  catch (err) {
+    console.log('erreur',err)
+    res.json({})
+  }
+})
 
+routeAPI.post('/equipements', async (req, res) => {
+  try{
+    const body = req.body
+    console.log("body is :", body)
+    await mesEquipements.create(body)
+    res.send("New equipe was created")
+  }
+  catch (err) {
+    console.log('erreur',err)
+    console.log("Creation did not work")
+    res.json({})
+  }
 })
